fb274193bd347f3c51adcb745e9cfb8439d16bf4
Terry Easley <easleter000@warren.k12.in.us>
2024-08-15 12:45:54 -0400

    U1L3.1.2

---
+++ src/speedcovert.java
@@ -0,0 +1,26 @@
+public class speedcovert {
+    public static void main(String[] args) {
+    System.out.println(toMilesPerHour(1.5));
+    System.out.println(toMilesPerHour(10.25));
+    System.out.println(toMilesPerHour(-5.6));
+    System.out.println(toMilesPerHour(25.42));
+    System.out.println(toMilesPerHour(75.114));
+    }
+
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if(kilometersPerHour < 0){
+            return -1;
+        }else{
+            double mph = kilometersPerHour / 1.609;
+            return Math.round(mph);
+
+
+        }
+
+    }
+
+
+
+
+}
